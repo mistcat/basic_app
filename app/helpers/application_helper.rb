f1d5f9a121c01dc4f26a77b7db2434194577e7ae
Nate Parsons <nparsons@phase2technology.com>
2012-02-16 13:34:43 -0500

    finished chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+  def logo(page_path)
+    logo = image_tag("logo.png", alt: "Sample App", class: "round") 
+    link_to logo, page_path
+  end
+
 end
